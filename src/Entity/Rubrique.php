ae37e67538dd9c4ce4312f711019daf14794864c
Jadczak christopher <christopher.jadczak@laposte.net>
2024-06-12 14:38:43 +0200

    database+entité

---
+++ src/Entity/Rubrique.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RubriqueRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RubriqueRepository::class)]
+class Rubrique
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 30)]
+    private ?string $libelle_court = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $libelle = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $image = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelleCourt(): ?string
+    {
+        return $this->libelle_court;
+    }
+
+    public function setLibelleCourt(string $libelle_court): static
+    {
+        $this->libelle_court = $libelle_court;
+
+        return $this;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): static
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
